f390488efc25c1960fcca5ceb0cbe063fcbc44af
Afsalazeez <afsalazeez505@gmail.com>
2018-09-03 21:11:20 +0530

    Added 2 functions to the InventoryActivity
    Created instances of both databaseHelper class and sqLiteDatabase. Added two functions those read from the database and write into the database

--- app/src/main/java/com/example/anjikkadans/inventoryapp/InventoryActivity.java
+++ app/src/main/java/com/example/anjikkadans/inventoryapp/InventoryActivity.java
@@ -1,13 +1,106 @@
 package com.example.anjikkadans.inventoryapp;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.anjikkadans.inventoryapp.data.InventoryContract;
+import com.example.anjikkadans.inventoryapp.data.InventoryDBHelper;
+
+/**
+ * This class in future may list all the items in the
+ * inventory with details
+ */
 public class InventoryActivity extends AppCompatActivity {
+
+    // declares the InventoryDBHelper class to access from
+    private InventoryDBHelper inventoryDBHelper;
+    // declares the SQLiteDatabase instance for the inventory database
+    private SQLiteDatabase inventoryDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+
+        // initializes the inventoryDBHelper class
+        inventoryDBHelper = new InventoryDBHelper(this);
+
+    }
+
+    // this function inserts some dummy data to the database
+    // when called
+    private void insertDummyData() {
+
+        // get the writeable type instance of inventory database
+        inventoryDatabase = inventoryDBHelper.getWritableDatabase();
+
+        // fake name for the dummy product
+        String productName = "Dummy Product";
+
+        // fake price for the dummy product
+        int productPrice = 100;
+
+        // fake quanitity for the dummy product
+        int quantity = 2;
+
+        // fake supplier name for the dummy product
+        String supplierName = "Fake Supplier";
+
+        // fake phone number of the supplier
+        String supplierPhoneNumber = "+91 8452******";
+
+        // creates and initializes ContentValues object to
+        // save data to the database
+        ContentValues contentValues = new ContentValues();
+
+        // putting values to the corresponding columns
+        contentValues.put(InventoryContract.InventoryFeedEntry.TABLE_NAME_INVENTORY, productName);
+        contentValues.put(InventoryContract.InventoryFeedEntry.COLUMN_PRICE, productPrice);
+        contentValues.put(InventoryContract.InventoryFeedEntry.COLUMN_QUANTITIY, quantity);
+        contentValues.put(InventoryContract.InventoryFeedEntry.COLUMN_SUPPLIER_NAME, supplierName);
+        contentValues.put(InventoryContract.InventoryFeedEntry.COLUMN_SUPPLIER_PHONE_NUMBER, supplierPhoneNumber);
+
+        // save the content values to the databse
+        inventoryDatabase.insert(InventoryContract.InventoryFeedEntry.TABLE_NAME_INVENTORY, null, contentValues);
+    }
+
+    // this function queries data from the database
+    // when called
+    private Cursor getData() {
+
+        // get the writeable type instance of inventory database
+        inventoryDatabase = inventoryDBHelper.getWritableDatabase();
+
+        // below string array contains the name of the columns to return
+        String[] projection = {InventoryContract.InventoryFeedEntry.COLUMN_PRODUCT_NAME,
+                InventoryContract.InventoryFeedEntry.COLUMN_QUANTITIY};
+
+        // below string holds conditions to filter the data
+        String selection = null;
+
+        // string array contains arguments to the above condition string
+        String[] selectionArgs = null;
+
+        // returns a cursor which holds data returned from the query
+        return inventoryDatabase.query(InventoryContract.InventoryFeedEntry.TABLE_NAME_INVENTORY,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                null);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // close the DBHelper class when the activity is
+        // destroyed
+        inventoryDBHelper.close();
     }
 }
